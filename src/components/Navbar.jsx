6f8e0932889aa91289e4cba60b89519fbf46efd5
Dilli_Rao <dilliwebdev218@gmail.com>
2024-08-11 14:38:12 +0530

    Minor Changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { AiOutlineClose, AiOUtlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
